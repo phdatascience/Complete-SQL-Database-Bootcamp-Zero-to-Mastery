e0082fb80b309f50da0627319df523107fd776cd
phdatascience <106822837+phdatascience@users.noreply.github.com>
2023-05-05 11:12:12 -0300

    Update questions.sql
    Answers from  BETWEEN + AND questions's

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -2,10 +2,14 @@
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+select firstname, lastname, age, income
+from customers
+where (age BETWEEN 30 and 50) and income < 50000;
 */
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
-* Write your query here
+select  avg(income)
+from customers
+where age BETWEEN 20 and 50;
 */
